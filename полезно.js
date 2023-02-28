8537a9385934f4eebb7e14d914e5c765d7fcb70a
RuslanMaratovich <104251991+RuslanMaratovich@users.noreply.github.com>
2023-02-28 21:57:58 +0400

    Delete полезно.js

--- полезно.js
+++
@@ -1,6 +0,0 @@
-//для ввода с клавиатуры
-//в консоль ввести: npm install prompt-sync
-
-const prompt = require("prompt-sync")({ sigint: true });
-const age = prompt("How old are you? ");
-console.log(`You are ${age} years old.`);
